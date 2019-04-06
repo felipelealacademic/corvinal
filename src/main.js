980f82e19209ac54c4fb3c3c99050a2f518d2d9b
Alisson Stopassole <alissonstopassole@hotmail.com>
2019-04-06 13:23:10 -0300

    Revert "configurtações do projeto"
    This reverts commit cef832455f864cd4bc3ee100dd1d385f24be138d.

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-// eslint-disable-next-line import/order
-import Axios from 'axios';
 
 Vue.use(BootstrapVue);
-Vue.prototype.$http = Axios;
+
 Vue.config.productionTip = false;
 
 new Vue({
